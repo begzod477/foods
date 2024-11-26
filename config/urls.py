bb4d9a0c5410e27c375f3ad85003fb032fb05bce
begzod477 <zokirovbegzod771@gmail.com>
2024-11-27 00:55:22 +0500

    updated

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', include('foods.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    path('', include('food.urls')),  
 ]
--- foods/urls.py
+++ foods/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('categories/', CategoryAPIView.as_view(), name='foodtype-list'),
     path('foods/', FoodAPIView.as_view(), name='food-list'),
     path('comments/', CommentAPIView.as_view(), name='comment-list'),
+    
 ]
