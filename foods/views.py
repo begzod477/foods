dea25dbdfc0f74b180442bb38d5fadcc0138c2b7
begzod477 <zokirovbegzod771@gmail.com>
2024-11-27 16:12:29 +0500

    first commit

--- foods/views.py
+++ foods/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from .models import Category, Food, Comment
@@ -15,18 +15,22 @@ class CategoryAPIView(GenericAPIView, ListModelMixin, CreateModelMixin):
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
-
-class FoodAPIView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    
+class FoodAPIView(ListAPIView): 
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
-    permission_classes = [IsAdminOrReadOnly]
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+# class FoodAPIView(GenericAPIView, ListModelMixin, CreateModelMixin):
+#     queryset = Food.objects.all()
+#     serializer_class = FoodSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 class CommentAPIView(GenericAPIView, ListModelMixin, CreateModelMixin):
